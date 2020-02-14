b6f0c5dd9d11e176d5a601b06e24ab69a5b9e388
tahuh <2011133049y@gmail.com>
2020-02-15 00:14:58 +0900

    initial

---
+++ javascript/functions.js
@@ -0,0 +1,12 @@
+function add2(val1, val2) {
+	return val1 + val2;
+}
+function sub2(val1, val2) {
+	return val1 - val2;
+}
+function mul2(val1, val2) {
+	return val1 * val2;
+}
+function div2(val1, val2) {
+	return val1 / val2
+}
