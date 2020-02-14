613a85af9b930ff97ecfb9017f06cedb77247943
tahuh <2011133049y@gmail.com>
2020-02-15 00:10:21 +0900

    initial commit

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -1,4 +1,12 @@
 var length = 16; // number
 var name = "Thomas" // string
-var obj = { name:"140", members:3}
+var obj = { 
+	name:"140", 
+	members:3,
+	echo : function() { // we will discuss later
+	return "140 is awesome team!";
+	} ,
+	myGroup:this.name // we will discuss this later
+}
 
+var arr = ["this" ,"is" ,"array" , "data", "type"]
