256eca3a499c56e1eea60a25684cb5092743fdcb
tahuh <2011133049y@gmail.com>
2020-02-14 23:20:38 +0900

    initial commit

---
+++ javascript/objects.js
@@ -0,0 +1,17 @@
+var car = {
+	type:"Flat",
+	model:"500",
+	color:"white"
+}
+
+// access perperties
+// accessing using dots(.)
+
+var carType = car.type
+var carModel = car.model
+var carColor = car.color
+
+// accessing using brackets([])
+var carType2 = car["type"]
+var carModel2 = car["model"]
+var carColor2 = car["color"]
