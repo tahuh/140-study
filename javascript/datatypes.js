005a842368814e3533d3af692a1cefda4277f353
tahuh <2011133049y@gmail.com>
2020-02-14 23:38:43 +0900

    initial commit

---
+++ javascript/datatypes.js
@@ -0,0 +1,4 @@
+var length = 16; // number
+var name = "Thomas" // string
+var obj = { name:"140", members:3}
+
---
+++ javascript/variables-and-operators.js
@@ -0,0 +1,21 @@
+// for variable named 'x' we assign numeric value '10'
+// for variable named 'y' we assign numeric value '20'
+var x = 10;
+var y = 20;
+
+// Arithematic operations
+var add = x + y;
+var sub = x - y;
+var mul = x * y;
+var div = x / y;
+var mod = x % y;
+var pow = x ** y;
+var inc = x++;
+var dec = y--;
+
+var z = 10;
+x == y
+x == z;
+x != z;
+x < 3;
+// etc...
