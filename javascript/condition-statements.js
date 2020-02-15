095a8816bea5804735e06d281f9ee9f0821b7840
tahuh <2011133049y@gmail.com>
2020-02-15 16:31:24 +0900

    initial

---
+++ javascript/condition-statements.js
@@ -0,0 +1,70 @@
+/* This is the most important lab
+ * As in most computer programming language
+ * JavaScript also offers condition statements
+ * So that we can divide things into conditions
+ */
+
+/*
+
+Conditional statements can be categorized into two groups
+
+ I. if/else group
+II. switch/case group
+
+ I. if/else group
+
+if (condition1) {
+	// when condition1 is true, process this block of code
+} else if (condition2) {
+	// when condition1 fails and condition2 is true then
+	// process this code block
+} else {
+	// when condition1 and condition2 fails and this is the other case
+	// process this block
+}
+
+NOTE. 
+A. else if can be used many times unless if statement is specified
+// below is valid
+
+if(some condition){
+
+} else if(other1){
+
+} else if(other2){
+
+} else if(other3){
+
+} ... many else ifs {
+
+} else {
+
+}
+
+
+// below is invalid
+// no if is present
+else if(some) {
+}  else if(other){
+} else if(other2){
+} ... else {
+}
+
+B. else can be omitted if not considered
+// below is valid
+if {
+}
+
+II. switch/case
+switch(expression) {
+	case A:
+		// do something
+		break; // always required this
+	case B:
+		// do with B
+		break;
+	default:
+		// when not all the case
+		break;
+}
+ */
