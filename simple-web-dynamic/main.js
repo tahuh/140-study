8aa01e4f6307454295af61ce3a061e6740fd6f05
tahuh <2011133049y@gmail.com>
2020-02-16 16:23:33 +0900

    changed code

--- simple-web-dynamic/main.js
+++ simple-web-dynamic/main.js
@@ -1,42 +1,54 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 var app = http.createServer( (req, res) => {
 	var _url = req.url;
-	var queryData = url.parse(_url, true).query
+	var queryData = url.parse(_url, true).query;
 	var title = queryData.id;
-	if(_url == '/'){
-		title = "Welcome";
+	var pathname = url.parse(_url, true).pathname;
+	if(pathname === '/'){
+		if(queryData.id === undefined){
+			title = "Welcome";
+		}
+		fs.readdir("data", (error, filelist) =>{
+			var liText = '';
+			var i = 0;
+			for(i = 0; i < filelist.length; i++){
+				var prefix = filelist[i].replace(".txt", "");
+				if(prefix === "Welcome"){
+					continue; // not showing on list
+				}
+				liText += `<li><a href="/?id=${prefix}">${prefix}</a></li>`
+			}
+			fs.readFile(`data/${title}.txt`, "utf8", (err, data)=>{
+				var template = `
+				<!DOCTYPE html>
+				<html>
+					<head>
+						<meta charset="utf-8">
+						<title>SimpleWebApp - ${title}</title>
+					</head>
+					<body>
+						<h1><a href="/?id=Welcome">WEB</a><h1>
+						<ol>
+							${liText}
+						</ol>
+						<h2>${title}</h2>
+						<p>
+							${data}
+						<p>
+					</body>
+				</html>
+				`;
+				res.writeHead(200);
+				res.end(template);
+			});
+		});
+	} else {
+		res.writeHead(404);
+		res.end("Not found");
 	}
-	if(_url == '/favicon.ico'){
-		return res.writeHead(404);
-	}
-	res.writeHead(200);
-	fs.readFile(`data/${title}.txt`, "utf8", (err, data)=>{
-	var template = `
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-			<title>SimpleWebApp - ${title}</title>
-		</head>
-		<body>
-			<h1><a href="/">WEB</a><h1>
-			<ol>
-				<li><a href="/?id=HTML">HTML</a></li>
-				<li><a href="/?id=CSS">CSS</a></li>
-				<li><a href="/?id=JavaScript">JavaScript</a></li>
-			</ol>
-			<h2>${title}</h2>
-			<p>
-				${data}
-			<p>
-		</body>
-	</html>
-	`;
-	res.end(template);
-	});
 });
 
 app.listen(3000);
